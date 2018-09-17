9c03ee91bf2e32978bc16b941b4c098d2ed57d7b
jonsung89 <jonsung89@gmail.com>
2018-09-17 18:39:56 -0500

    Added POST login endpoint without jwt token setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -39,4 +39,31 @@ router.post('/register', (req, res) => {
     })
 });
 
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ username })
+    .then(user => {
+      // Check for user
+      if(!user) {
+        return res.status(404).json({ username: 'Username not found' });
+      }
+
+      // Check Password
+      bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if(isMatch) {
+          res.json({ msg: 'Success' });
+        } else {
+          return res.status(400).json({ password: 'Password incorrect' });
+        }
+      })
+    });
+})
+
 module.exports = router;
