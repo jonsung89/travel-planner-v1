630ef373d831d1d70b65eae789fb25a9428437ba
jonsung89 <jonsung89@gmail.com>
2018-09-21 11:09:08 -0500

    preparing to deploy

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+};
---
+++ config/passport.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    // console.log(jwt_payload); // GET request to api/users/current with the working bearer token returns user's payload including id, username, email
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if(user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      })
+      .catch(err => console.log(err));
+  }));
+}
